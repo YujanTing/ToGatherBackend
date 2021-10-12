e28f0299c49ba08aa9b6fc36646d4ba042f6f1bd
YujanTing <yu-jan.ting@emory.edu>
2021-10-11 20:56:01 -0400

    initial codes

--- togather/events/models.py
+++ togather/events/models.py
@@ -1,16 +1,30 @@
 from django.db import models
+
 # from django.contrib.auth.models import User
 
 EventTypes = [
-    (0,"Chinese"),
-    (1,"Japanese"),
-    (2,"Korean"),
-    (3,"Thai")
+    (0, "Chinese"),
+    (1, "Japanese"),
+    (2, "Korean"),
+    (3, "Thai")
 ]
 
-States = [
-    (0,"GA")
-]
+States = [('AL', 'Alabama'),
+ ('AK', 'Alaska'),
+ ('AS', 'American Samoa'), ('AZ', 'Arizona'), ('AR', 'Arkansas'), ('AA', 'Armed Forces Americas'),
+ ('AE', 'Armed Forces Europe'), ('AP', 'Armed Forces Pacific'), ('CA', 'California'), ('CO', 'Colorado'),
+ ('CT', 'Connecticut'), ('DE', 'Delaware'), ('DC', 'District of Columbia'), ('FL', 'Florida'), ('GA', 'Georgia'),
+ ('GU', 'Guam'), ('HI', 'Hawaii'), ('ID', 'Idaho'), ('IL', 'Illinois'), ('IN', 'Indiana'), ('IA', 'Iowa'),
+ ('KS', 'Kansas'), ('KY', 'Kentucky'), ('LA', 'Louisiana'), ('ME', 'Maine'), ('MD', 'Maryland'),
+ ('MA', 'Massachusetts'), ('MI', 'Michigan'), ('MN', 'Minnesota'), ('MS', 'Mississippi'), ('MO', 'Missouri'),
+ ('MT', 'Montana'), ('NE', 'Nebraska'), ('NV', 'Nevada'), ('NH', 'New Hampshire'), ('NJ', 'New Jersey'),
+ ('NM', 'New Mexico'), ('NY', 'New York'), ('NC', 'North Carolina'), ('ND', 'North Dakota'),
+ ('MP', 'Northern Mariana Islands'), ('OH', 'Ohio'), ('OK', 'Oklahoma'), ('OR', 'Oregon'), ('PA', 'Pennsylvania'),
+ ('PR', 'Puerto Rico'), ('RI', 'Rhode Island'), ('SC', 'South Carolina'), ('SD', 'South Dakota'), ('TN', 'Tennessee'),
+ ('TX', 'Texas'), ('UT', 'Utah'), ('VT', 'Vermont'), ('VI', 'Virgin Islands'), ('VA', 'Virginia'), ('WA', 'Washington'),
+ ('WV', 'West Virginia'), ('WI', 'Wisconsin'), ('WY', 'Wyoming')]
+
+
 # Create your models here.
 class Event(models.Model):
     event_type = models.SmallIntegerField(blank=False, choices=EventTypes, verbose_name="Culture")
